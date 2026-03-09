7477e555183cc61dbccc60546d781eca30e24782
PariSenjaliya <paricollege239@gmail.com>
2026-03-10 00:35:50 +0530

    Add  Practical03.java program
     Set-01 practical03.java program

---
+++ SET-01(Basics of JAVA)/Practical03.java
@@ -0,0 +1,16 @@
+package pkg250763107023;
+
+public class practical03 {
+
+    public static void main(String[] args) {
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Sum = " + sum);
+    }
+}
