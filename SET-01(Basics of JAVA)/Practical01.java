dbf4a40ccdfd040948117b36fe96b954d4836782
PariSenjaliya <paricollege239@gmail.com>
2026-03-10 00:27:52 +0530

    Add  Practical01.java program
    Set-01 practical01.java program

---
+++ SET-01(Basics of JAVA)/Practical01.java
@@ -0,0 +1,42 @@
+package pkg250763107023;
+
+import java.util.Scanner;
+
+public class practical01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter amount: ");
+        int amount = sc.nextInt();
+
+        int note100, note50, note10, note5, note2, note1;
+
+        note100 = amount / 100;
+        amount = amount % 100;
+
+        note50 = amount / 50;
+        amount = amount % 50;
+
+        note10 = amount / 10;
+        amount = amount % 10;
+
+        note5 = amount / 5;
+        amount = amount % 5;
+
+        note2 = amount / 2;
+        amount = amount % 2;
+
+        note1 = amount;
+
+        System.out.println("\nMinimum number of notes required:");
+        System.out.println("100 = " + note100);
+        System.out.println("50 = " + note50);
+        System.out.println("10 = " + note10);
+        System.out.println("5 = " + note5);
+        System.out.println("2 = " + note2);
+        System.out.println("1 = " + note1);
+
+        sc.close();
+    }
+}
